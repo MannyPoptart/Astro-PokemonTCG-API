6439509e45d3d4b0f60b23d9376f916072cca73e
Manny Aguilera <maguilera5@ewu.edu>
2024-05-24 20:54:41 +0000

    Can now select a pokemon tcg set to display cards in that set

--- src/scripts/pokemonFinder.js
+++ src/scripts/pokemonFinder.js
@@ -1,27 +1,60 @@
-// const key = 'bea89b29-a96f-4bf0-ab54-59373caedba4 ';
+// const key = 'bea89b29-a96f-4bf0-ab54-59373caedba4';
 
-document.getElementById('pokemon-form').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevents the form from submitting the traditional way
-  const pokemonID = document.getElementById('pokemon-id-input').value;
-  
-  const data = 'https://api.pokemontcg.io/v2/cards/' + pokemonID;
+// Data for sets: https://api.pokemontcg.io/v2/sets
 
-  const section = document.querySelector('section');
+const sets = 'https://api.pokemontcg.io/v2/sets';
 
+// Fetch the sets and put them into options in order by date released
+fetch(sets)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    const sets = data.data;
+    const select = document.getElementById('pokemon-sets');
+    sets.sort((a, b) => {
+      return new Date(a.releaseDate) - new Date(b.releaseDate);
+    });
+    sets.forEach(set => {
+      const option = document.createElement('option');
+      option.value = set.id;
+      option.text = set.name;
+      select.appendChild(option);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+});
+
+// Fetch the cards from the selected set and display them in the section called container, add a button to then display the cards of the selected set
+document.getElementById('pokemon-sets').addEventListener('change', function() {
+  const setID = this.value;
+  const data = 'https://api.pokemontcg.io/v2/cards?q=set.id:' + setID;
+
+  const container = document.querySelector('.container');
+  container.innerHTML = '';
+
+  // Display each card in order by number in the set 1,2,3, etc.
   fetch(data)
     .then(response => {
       return response.json();
     })
-    .then(card => {
-      console.log(card);
-      const temp = `
-        <ul class='card'>
-          <li>${card.data.name}</li>
-          <img class='pokeIMG' src="${card.data.images.large}" alt="">
-          <li class='author'>Author: ${card.data.artist}</li>
-        </ul>
-      `;
-      section.insertAdjacentHTML('afterbegin', temp);
+    .then(data => {
+      const cards = data.data;
+      cards.sort((a, b) => {
+        return a.number - b.number;
+      });
+      cards.forEach(card => {
+        const temp = `
+          <ul class='card'>
+            <li>${card.name}</li>
+            <img class='pokeIMG' src="${card.images.large}" alt="">
+            <li class='author'>Author: ${card.artist}</li>
+            <li class='cardNum'>Card Number: ${card.number}</li>
+          </ul>
+        `;
+        container.insertAdjacentHTML('beforeend', temp);
+      });
     })
     .catch(error => {
       console.error('Error fetching data:', error);
