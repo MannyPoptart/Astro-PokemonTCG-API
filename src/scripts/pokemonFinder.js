db3b85c4413cf4b29cc369d095b69d8e710e0c47
Manny Aguilera <maguilera5@ewu.edu>
2024-05-27 15:03:51 +0000

    Trying to fix when a set has more than 250 cards

--- src/scripts/pokemonFinder.js
+++ src/scripts/pokemonFinder.js
@@ -40,12 +40,78 @@ document.getElementById('pokemon-sets').addEventListener('change', function() {
       return response.json();
     })
     .then(data => {
+      const setTotalCards = data;
       const cards = data.data;
+      // console.log(setTotalCards.totalCount);
       cards.sort((a, b) => {
         return a.number - b.number;
       });
       cards.forEach(card => {
-        if (card.types && card.types.length >= 1) { // Check if card.types is defined
+        if (card.types && card.types.length == 1) { // Check if card.types is defined
+          const temp = `
+            <ul class='card' style='border-color: ${colors.Colors[card.types[0]]};'>
+              <li>${card.name}</li>
+              <li class='types'>Type: ${card.types[0]}</li>
+              <img class='pokeIMG' src="${card.images.large}" alt="">
+              <li class='artist'>Artist: ${card.artist}</li>
+              <li class='cardNum'>Card #: ${card.number} / ${card.set.total}</li>
+              <li class='weakness'>${card.weaknesses ? 'Weakness: ' + card.weaknesses[0].type : 'No Weakness'}</li>
+            </ul>
+          `;
+          container.insertAdjacentHTML('beforeend', temp);
+        } else if (card.types && card.types.length == 2) {
+          const temp = `
+            <ul class='card' style='border-image: linear-gradient(to bottom right, ${colors.Colors[card.types[0]]} 10%, ${colors.Colors[card.types[1]]} 100%) 1;'>
+              <li>${card.name}</li>
+              <li class='types'>Types: ${card.types[0]} & ${card.types[1]}</li>
+              <img class='pokeIMG' src="${card.images.large}" alt="">
+              <li class='artist'>Artist: ${card.artist}</li>
+              <li class='cardNum'>Card #: ${card.number} / ${card.total}</li>
+              <li class='weakness'>Weakness: ${card.weaknesses[0].type}</li>
+            </ul>
+          `;
+          container.insertAdjacentHTML('beforeend', temp);
+        } else {
+          const temp = `
+            <ul class='card' style='border-color: ${colors.Colors['Trainer']};'>
+              <li>${card.name}</li>
+              <li class='types'>Type: Trainer</li>
+              <img class='pokeIMG' src="${card.images.large}" alt="">
+              <li class='artist'>Artist: ${card.artist}</li>
+              <li class='cardNum'>Card #: ${card.number} / ${card.set.total}</li>
+            </ul>
+          `;
+          container.insertAdjacentHTML('beforeend', temp);
+        }
+
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+});
+
+// If the set has more than 250 cards, display a button which will take the next page of cards to display in order by number
+document.getElementById('nextPage').addEventListener('click', function() {
+  const setID = document.getElementById('pokemon-sets').value;
+  const data = 'https://api.pokemontcg.io/v2/cards?q=set.id:' + setID + '&page=2';
+
+  const container = document.querySelector('.container');
+  container.innerHTML = '';
+
+  fetch(data)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      const cards = data.data;
+      // Make nextPage not hidden
+      document.getElementById('nextPage').style.display = 'block';
+      cards.sort((a, b) => {
+        return a.number - b.number;
+      });
+      cards.forEach(card => {
+        if (card.types && card.types.length == 1) {
           const temp = `
             <ul class='card' style='border-color: ${colors.Colors[card.types[0]]};'>
               <li>${card.name}</li>
